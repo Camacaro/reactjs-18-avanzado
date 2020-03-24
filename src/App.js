677e018d95259fd86a1cf9e4d7b27cc20b20fde8
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-03-24 15:26:52 -0300

    photo cards and logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { Category } from './components/Category'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 
 export const App = () => (
     <div>
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
+        <ListOfPhotoCards />
     </div>
 )
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,13 +2,22 @@
 import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
+import { categories } from '../../../api/db.json'
 
  
 export const ListOfCategories = () => {
     return (
         <List>
             {
-                [1, 2, 3, 4].map( category => <Item key={category}> <Category /> </Item>)
+                categories.map( category => 
+                
+                    <Item key={category.id}> 
+
+                        {/* <Category cover={category.cover} emoji={category.emoji} />  */}
+                        <Category {...category} /> 
+                    
+                    </Item>
+                )
             }
         </List>
     )
