4d2ea340fe97acdc45bc4a80229a7055b71a3632
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-03-25 08:06:40 -0300

    lazy lout with hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -12,27 +12,31 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
     const [show, setShow] = useState(false);
 
     useEffect( function() {
-
         // console.log(articleRef.current);
 
         // con esta funcion lo que hicimos es renderizar tipo un lazy load
 
-        // con esta funcion vamos a ver todas las entreas qeu se encuentre en el viewport
-        const observer = new window.IntersectionObserver(function (entries) {
-            // console.log(entries);
+        // import dinamico, usamos este para que sea compatible con todos los navegadores
+        import('intersection-observer')
+        .then( () => {
+            // con esta funcion vamos a ver todas las entreas qeu se encuentre en el viewport
+            const observer = new window.IntersectionObserver(function (entries) {
+                // console.log(entries);
 
-            const { isIntersecting } = entries[0];
-            // console.log(isIntersecting);
-            if ( isIntersecting ) {
-                console.log('si');
-                setShow(true)
+                const { isIntersecting } = entries[0];
+                // console.log(isIntersecting);
+                if ( isIntersecting ) {
+                    console.log('si');
+                    setShow(true)
 
-                // evitar que se vuelva actualizar 
-                observer.disconnect()
-            }
+                    // evitar que se vuelva actualizar 
+                    observer.disconnect()
+                }
+            })
+
+            observer.observe( articleRef.current )
+
         })
-
-        observer.observe( articleRef.current )
 
         // se ejecutara solo cuando cambie la referencia
     }, [articleRef]);
