f5a3bb5ee71909001bd586007e029fc3f3d60c04
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-03-27 10:10:38 -0300

    deatils

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { GlobalStyle } from '../src/styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+// import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
+import { Detail } from './pages/Detail'
+
 
 export const App = () => {
     
@@ -17,14 +19,21 @@ export const App = () => {
         <div>
             <GlobalStyle />
             <Logo />
-            {
+
+            <Router>
+                <Home path='/' />
+                <Home path='/pet/:id' />
+                <Detail path='/detail/:detailId' />
+            </Router> 
+
+            {/* {
                 detailId 
                 ? <PhotoCardWithQuery id={detailId} />
                 : <Router>
                     <Home path='/' />
                     <Home path='/pet/:id' />
                 </Router> 
-            }
+            } */}
         </div>
     )
 } 
---
+++ src/pages/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { PhotoCardWithQuery } from '../container/PhotoCardWithQuery';
+
+PhotoCardWithQuery
+export const Detail = ({detailId}) => {
+    return (
+        <PhotoCardWithQuery id={detailId} />
+    );
+};
